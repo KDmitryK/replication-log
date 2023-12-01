b7cc9bf8cbe1a3db5fceabb1c35f4ff49af74ea4
Dmitry <kulyomin.dmitry@gmail.com>
2023-12-01 22:08:41 +0200

    Exception logging in services

--- master/src/main/java/com/ds/replicationlog/master/controller/ExceptionsHandler.java
+++ master/src/main/java/com/ds/replicationlog/master/controller/ExceptionsHandler.java
@@ -1,5 +1,7 @@
 package com.ds.replicationlog.master.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,14 +11,17 @@ import java.util.concurrent.TimeoutException;
 
 @ControllerAdvice
 public class ExceptionsHandler {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler({TimeoutException.class})
     public ResponseEntity<Object> handleTimeoutException(TimeoutException exception) {
+        logger.warn("Request has failed because of timeout", exception);
         return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("Timeout during request processing");
     }
 
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        logger.warn("Request has failed because of internal error", exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal problem");
     }
 }
--- slave/src/main/java/com/ds/replicationlog/slave/controller/ExceptionsHandler.java
+++ slave/src/main/java/com/ds/replicationlog/slave/controller/ExceptionsHandler.java
@@ -1,5 +1,7 @@
 package com.ds.replicationlog.slave.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionsHandler {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        logger.warn("Request has failed because of internal error", exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal problem");
     }
 }
